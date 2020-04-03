18b746ebbf25589180da9f38e335bb7f2f41fec4
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2020-04-04 01:51:41 +0545

    Changes: API, API Auth, Api Configuration, Index Page Fulfilled with Popular and Now Showing Movies, Single Show Details Page Completed.

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,43 +5,56 @@
     <div class="movie-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
             <!-- Image Here-->
-            <img src="/img/movies/knivesout.jpg" alt="Knives Out" class="w-custom-20">
+            <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="{{ $movie['title'] }} Poster" class="w-custom-20">
 
             <!-- Details Here -->
             <div class="md:ml-24">
-                <h2 class="text-4xl font-semibold">Knives Out</h2>
+                <h2 class="text-4xl font-semibold">{{ $movie['title'] }}</h2>
 
                 <div class="flex flex-wrap items-center text-gray-400 text-sm">
                     <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
-                    <span class="ml-2">85%</span>
+                    <span class="ml-2">{{ $movie['vote_average'] * 10 . '%' }}</span>
                     <span class="mx-2">|</span>
-                    <span>Feb 20, 2020</span>
+                    <span> {{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y') }}</span>
                     <span class="mx-2">|</span>
-                    <span class="">Action, Thriller, Comedy</span>
+                    <span class="">
+                        @foreach( $movie['genres'] as $genre)
+                            {{ $genre['name'] }}@if(!$loop->last), @endif
+                        @endforeach
+                    </span>
                 </div>
 
-                <p class="text-gray-300 mt-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus accusantium aspernatur blanditiis consequuntur corporis cupiditate doloremque eaque eos esse fugiat hic impedit ipsum itaque iusto officiis, optio perspiciatis placeat praesentium quam quasi, quo reiciendis sapiente vitae voluptate. Adipisci asperiores eveniet expedita in ipsa iure iusto, maxime sed velit voluptatem.</p>
+                <p class="text-gray-300 mt-8">
+                    {{ $movie['overview'] }}
+                </p>
 
                 <div class="mt-12">
                     <h4 class="text-white font-semibold">Featured Cast</h4>
                     <div class="flex mt-4">
-                        <div>
-                            <div>Chris Evans</div>
-                            <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
-                        </div>
-                        <div class="ml-8">
-                            <div>Katherine Langford</div>
-                            <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
-                        </div>
+
+                        @foreach($movie['credits']['crew'] as $crew)
+                            @if($loop->index < 2)
+                                <div class="mr-8">
+                                    <div> {{ $crew['name'] }} </div>
+                                    <div class="text-sm text-gray-400"> {{ $crew['job'] }} </div>
+                                </div>
+                            @endif
+                        @endforeach
+
                     </div>
                 </div>
 
-                <div class="mt-12">
-                    <button class="flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-                        <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
-                        <span class="ml-2">Play Trailer</span>
-                    </button>
-                </div>
+                @if(count($movie['videos']['results']) > 0)
+
+                    <div class="mt-12">
+                        <a href="https://youtube.com/watch?v={{$movie['videos']['results'][0]['key']}}" class="flex items-center inline-flex bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                            <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+                            <span class="ml-2">Play Trailer</span>
+                        </a>
+                    </div>
+
+                @endif
+
             </div>
         </div>
     </div>
@@ -52,17 +65,39 @@
             <h4 class="text-4xl font-semibold">Cast</h4>
             <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
 
-                <div class="mt-4">
-                    <a href="">
-                        <img src="/img/casts/kathlove.jpg" alt="Katherine Langford" class="hover:opacity-75 transition ease-in-out duration-150">
-                    </a>
-                    <div class="mt-2">
-                        <a href="" class="mt-2 text-lg hover:text-gray-300">Katherine Langford</a>
+                @foreach($movie['credits']['cast'] as $cast)
+                    @if($loop->index < 6)
+                        <div class="mt-4">
+                            <a href="">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w300/'.$cast['profile_path'] }}" alt="{{ $movie['title'] }} Images" class="hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="mt-2">
+                                <a href="" class="mt-2 text-lg hover:text-gray-300">{{ $cast['name'] }}</a>
+                            </div>
+                            <div class="text-gray-400 text-sm">
+                                {{ $cast['character'] }}
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Images Section Here -->
+    <div class="movie-cast border-b border-gray-800">
+        <div class="container mx-auto px-4 py-8">
+            <h4 class="text-4xl font-semibold">Images</h4>
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+
+                @foreach($movie['images']['backdrops'] as $image)
+                    <div class="mt-4">
+                        <a href="">
+                            <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="Katherine Langford" class="hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
                     </div>
-                    <div class="text-gray-400 text-sm">
-                        Mary Jane
-                    </div>
-                </div>
+                @endforeach
 
             </div>
         </div>
